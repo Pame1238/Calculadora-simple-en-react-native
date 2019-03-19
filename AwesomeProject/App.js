2ce3e0f9d60a382bf233dd1d8285a021585a6b5d
Pame1238 <32856495+Pame1238@users.noreply.github.com>
2019-03-18 21:44:42 -0300

    0.0
    0.0

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import {View, Button, Text} from 'react-native'
+import NumericInput from 'react-native-numeric-input'
+
 
 
 export default class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      value1: '',
+      value2: ''
+    }
+  }
+
+  
+
   render() {
-    let pic  = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <Image source={pic} style={{width: 193, height: 110}}></Image>
+      <View style={{padding: 50}}>
+        <Text>Tipica calculadora by Pame</Text>
+        <NumericInput   
+          type='up-down'  
+          onChange={(text) => this.setState({value1: text})}
+         />
+
+        <NumericInput 
+          type='up-down' 
+          onChange={(text) => this.setState({value2: text})} 
+        />
+
+        <Button title="Suma" onPress={(value1,value2) => {
+          value1= this.state.value1
+          value2 = this.state.value2
+          alert(value1+value2)
+        }}/>
+
+        <Button title="Resta" onPress={(value1,value2) => {
+          value1= this.state.value1
+          value2 = this.state.value2
+          alert(value1-value2)
+        }}/>
+
+        <Button title="Multiplicacion" onPress={(value1,value2) => {
+          value1= this.state.value1
+          value2 = this.state.value2
+          alert(value1*value2)
+        }}/>
+
+        <Button title="Division" onPress={(value1,value2) => {
+          value1= this.state.value1
+          value2 = this.state.value2
+          alert(value1/value2)
+        }}/>
+        
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
